14f114619ae44e66f57abf9c47db5a40d5504a4d
sungHeeLee <70899677+hee9841@users.noreply.github.com>
2025-01-16 15:37:10 +0900

    Refactor: ch02 코드 리팩터링 (#1)
    * Refactor: ch02 main 코드 함수로 refactor
    
    * Refactor: ch02의 logic_ex1 함수에서 엔티티 메니저 관리하게 변경

--- src/main/java/com/jpabook/Main.java
+++ src/main/java/com/jpabook/Main.java
@@ -10,47 +10,53 @@ public class Main {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        //cho02
+//        logic_ex1(emf);
+
+
+        emf.close();
+    }
+
+    private static void logic_ex1(EntityManagerFactory emf) {
+
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
 
+        tx.begin();
         try {
             //저장
-//            logic_save(em);
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("member1");
+            em.persist(member);
 
-            //조회 & update
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("newName");
+
+            //조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("member name: " + findMember.getName());
+
+            //update
+            findMember.setName("newName");
+            Member findMember2 = em.find(Member.class, 1L);
+            System.out.println("update member name: " + findMember2.getName());
+
 
             //JPQL 전체 맴버 조회?? ->
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
                 .getResultList();
-
-            //페이ㅣ지 네이션 setFirtResult, setMasResults..
-
-            //테이블 대상이 아니라, 객체임
-            //sql -> 테이블 대상으로 쿼리
-            //jpql -> 객체 대상으로 쿼리, 객체지향 sql
 
             for (Member m : result) {
                 System.out.println(m.getName());
             }
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
         }
-
-        emf.close();
     }
 
-    private static void logic_save(EntityManager em) {
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("member1");
-        em.persist(member);
-    }
 }
